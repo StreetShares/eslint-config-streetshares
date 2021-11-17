638b5259ae43f1eb3e4076218f960a5f7e4536bc
Tom Baker <tbaker@streetshares.com>
2021-11-17 15:32:06 -0500

    Increase timeout correctly >.<

--- test/index.test.js
+++ test/index.test.js
@@ -3,7 +3,7 @@ const { ESLint } = require('eslint');
 var assert = require('assert');
 
 describe('Validate ESLint configs', () => {
-  this.timeout = 5000; // increase default timeout because initializing the linter can take more than 2 seconds
+  this.timeout(5000); // increase default timeout because initializing the linter can take more than 2 seconds
   const overrideConfigFile = 'index.js';
   const cli = new ESLint({
     overrideConfigFile,
